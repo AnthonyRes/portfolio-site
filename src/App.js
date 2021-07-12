d75955fe2b288c126ca657d14c17e81c9c53dc6e
Anthony Resnick <resnick.anthony@gmail.com>
2021-07-12 16:32:58 -0400

    'added more styling to landing'

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
 
-import { Jumbotron } from 'react-bootstrap';
+import { Col, Jumbotron, Row } from 'react-bootstrap';
+
 import ModalHeader from 'react-bootstrap/esm/ModalHeader';
 import React from 'react'
 
 function App() {
   return (
-    <div className="background">
-      <Jumbotron className='title' fluid>
-        <h1 className='titleContent'>Anthony Resnick</h1>
-      </Jumbotron>
-    </div>
-
+    <>
+      <Row className="background">
+        <Col>
+          <Jumbotron className='title' fluid>
+            <h1 className='titleContent'>Anthony Resnick</h1>
+          </Jumbotron>
+        </Col>
+      </Row>
+      <Row>
+        <div className='tag'>
+          <p>coming soon...</p>
+        </div>
+      </Row>
+    </>
   );
 }
 
